7f84fca3b4094400692dea91908445b371d392a4
abhihash01 <abhilash29998@gmail.com>
2024-01-20 17:41:48 -0500

    adding clear knowledge base functionality and handling cases when knowledge base is absent

--- app.py
+++ app.py
@@ -3,22 +3,25 @@ from ingest import ingestor
 from input_processor import input_processor
 from prompt_conversatoin_chain import conversational_chain
 import validators
+import shutil
+import os
+faiss_path = 'FAISS_store'
 class app:
     def run(self):
         ingest=ingestor()
         st.set_page_config("Fine tuned Rag applicaiton")
-        st.header("RAG Applicaiton")
+        st.header("Fine Tuned RAG Applicaiton")
 
 
         with st.sidebar:
             st.title("Knowledge Bases")
-            st.caption("No api required for enterprise models. Don't add both urls and pdfs together")
+            st.caption("No api required for enterprise models")
             model_temp= st.selectbox("Select Model", ("Gemini", "OpenAI","LLama","Mistral"))
             urls=pdf=None
-            urls = st.text_input("URL", label_visibility="collapsed")
+            urls = st.text_input("Enter URLs here")
             pdfs= st.file_uploader("Upload PDFs",accept_multiple_files=True)
             if st.button("Store Knowledge"):
-                with st.spinner("loading..."):
+                with st.spinner("Creating Knowledge Base"):
                     if pdfs:
                         try:
                             raw_text=ingest.extract_pdf_text(pdfs)
@@ -38,7 +41,20 @@ class app:
                             ingest.store_vectors(text_chunks)
 
 
-                    st.success("Done")
+                    st.success("Knowledge Base Created")
+            st.caption("Don't add both urls and pdfs together")
+            if st.button("Clear Knowledge Base"):
+                with st.spinner("Clearing Knowledge Base"):
+                    if not os.path.exists(faiss_path):
+                        st.write("Knowledge Base Already Clear")
+                    else:
+                        try:
+
+                            shutil.rmtree(faiss_path)
+                        except Exception as e:
+                            st.exception(f"Exception: {e}")
+                st.success("Knowledge Base Cleared")
+
         
         question = st.text_input("Enter Question here")
 
@@ -55,10 +71,18 @@ class app:
             response = inp_processor.get_response(chat,question)
             st.write("Response: ", response.content)
         if submit2:
-            chain = conver_chain.load_chain()
-            docs = inp_processor.get_similar_docs(question)
-            response = inp_processor.get_knowledge_response(chain,docs,question)
-            st.write("Response: ", response["output_text"])
+            #check if knowledge base exists
+            KB_exists = inp_processor.KB_exists()
+            if question:
+                if KB_exists:
+                    chain = conver_chain.load_chain()
+                    docs = inp_processor.get_similar_docs(question)
+                    response = inp_processor.get_knowledge_response(chain,docs,question)
+                    st.write("Response: ", response["output_text"])
+                else:
+                    st.write("Response: The Knowledge Base Doesn't Exist. Create Knowledge Base to use this feature")
+            else:
+                st.write("Response: The question field is empty. Enter Question")
 
 
         
--- input_processor.py
+++ input_processor.py
@@ -2,12 +2,17 @@ import os
 from dotenv import load_dotenv
 from langchain_community.vectorstores import FAISS
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
+faiss_path="FAISS_store"
 class input_processor:
     def __init__(self) -> None:
         self.gemini_embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
     
-    def get_similar_docs(self,question,faiss_path="FAISS_store"):
+    def KB_exists(self,faiss_path = faiss_path):
+        if os.path.exists(faiss_path):
+            return True
+        else:
+            return False
+    def get_similar_docs(self,question,faiss_path=faiss_path):
         faiss_db = FAISS.load_local(faiss_path,self.gemini_embeddings)
         docs=faiss_db.similarity_search(question)
         return docs
